dc546dc3b8e5d524b5d1eaee7dd16fb211e232dd
Borja González <borjago.bgo@gmail.com>
2023-12-27 20:21:01 +0100

    tests and refactor

---
+++ teams/teams.controller.js
@@ -0,0 +1,35 @@
+let teamsDatabase = {};
+const cleanUpTeam = () => {
+    for(let user in teamsDatabase){
+        teamsDatabase[user] = [];
+    }
+}
+
+const bootstrapTeam = (userId) =>{
+    teamsDatabase[userId] = [];
+}
+
+const getTeamOfUser = (userId) =>{
+    return teamsDatabase[userId]
+}
+
+const addPokemon = (userId, pokemon) => {
+    teamsDatabase[userId].push(pokemon);
+}
+const deletePokemon = (userId, index) =>{
+    console.log('DELETE', userId, index);
+    if(teamsDatabase[userId][index]) {
+        teamsDatabase[userId].splice(index, 1)
+    }
+}
+
+const setTeam = (userId, team) => {
+    teamsDatabase[userId] = team;
+}
+
+exports.bootstrapTeam = bootstrapTeam;
+exports.addPokemon = addPokemon;
+exports.deletePokemon = deletePokemon;
+exports.setTeam = setTeam;
+exports.getTeamOfUser = getTeamOfUser;
+exports.cleanUpTeam = cleanUpTeam;
---
+++ teams/teams.router.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+require('../tools/auth.controller')(passport);
+const axios = require('axios').default;
+
+const teamsController = require('./teams.controller');
+const { getUser } = require('../auth/users.controller');
+
+router.route('/')
+    .get( passport.authenticate('jwt', {session: false}), 
+            (req, res) => {
+                let user = getUser(req.user.userId);
+                res.status(200).json({
+                    trainer: user.userName,
+                    team: teamsController.getTeamOfUser(req.user.userId)
+                })
+        })
+    .put( passport.authenticate('jwt', {session: false}),
+        (req, res) => {
+        teamsController.setTeam(req.user.userId, req.body.team);
+        res.status(200).send();
+    })
+
+router.route('/pokemons')
+    .post(passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        let pokemonName = req.body.name;
+        console.log('calling pokeapi');
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+            .then(function (response){
+                //handle success
+                let pokemon = {
+                    name: pokemonName,
+                    pokedexNumber: response.data.id
+                }
+                teamsController.addPokemon(req.user.userId, pokemon);
+                res.status(201).send(pokemon);
+            })
+            .catch(function (error) {
+                //handle error
+                res.status(400).json({message: error});
+                console.log(error);
+            })
+            .then(function (){
+                //always executed
+            })
+    })
+
+router.route('/pokemons/:pokeid')
+    .delete(passport.authenticate('jwt', {session: false}), (req, res) => {
+        teamsController.deletePokemon(req.user.userId, req.params.pokeid);
+        res.status(200).send('Hello World!')
+    })
+
+exports.router = router;
